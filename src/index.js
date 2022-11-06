2f2beeb851ea0dc7050b4536f0c92aa0e4797287
Nafisa <nafiritzy.59@gmail.com>
2022-11-06 20:10:30 +0530

    Add loading while fetching

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import weather from './weather';
 import DOM from './DOM';
 import img from './smilingsun.png';
 
-
 document.querySelector('.loading').src = img;
+const loading = document.querySelector('#loading-icon');
 weather.fetchWeather('Tokyo').then((data) => {
   const {
     name, icon, main, temp, humidity, feels_like, speed,
   } = data;
   DOM.changeCard(name, temp, icon, main, humidity, speed, feels_like);
+  loading.classList.add('invisible');
 });
 async function changeDisplay() {
+  loading.classList.remove('invisible');
   const searchBar = document.querySelector('.search-bar');
   DOM.changeBackground(searchBar.value);
   weather.fetchWeather(searchBar.value).then((data) => {
@@ -19,6 +21,7 @@ async function changeDisplay() {
       name, icon, main, temp, humidity, feels_like, speed,
     } = data;
     DOM.changeCard(name, temp, icon, main, humidity, speed, feels_like);
+    loading.classList.add('invisible');
   });
 }
 
